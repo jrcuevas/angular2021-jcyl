0731067a37a4f769a07d9a836f50b49721112e29
jrcuevas <15145883+jrcuevas@users.noreply.github.com>
2021-06-04 19:49:47 +0200

    Ejercicio 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoSolicitudesComponent } from './listado-solicitudes/listado-solicitudes.component';
+import { SolicitudComponent } from './solicitud/solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListadoSolicitudesComponent},
+  {path: 'solicitud', component: SolicitudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
