16f9685dcfc51d9e364b7b03ecc1cf87a4596b66
jrcuevas <15145883+jrcuevas@users.noreply.github.com>
2021-06-15 22:56:30 +0200

    Ejercicios 21 & 22

--- src/app/datos-bancarios/datos-bancarios.component.ts
+++ src/app/datos-bancarios/datos-bancarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-datos-bancarios',
@@ -12,7 +13,7 @@ export class DatosBancariosComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submit(f: any){
+  submit(f: NgForm){
     console.log(f);
   }
 
