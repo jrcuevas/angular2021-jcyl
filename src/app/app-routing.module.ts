4fdf38b6b184b03e35e2e664427b327c21f59121
jrcuevas <15145883+jrcuevas@users.noreply.github.com>
2021-06-17 19:29:51 +0200

    Ejercicio 28

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './authentication.guard';
 import { DatosBancariosCComponent } from './datos-bancarios-c/datos-bancarios-c.component';
 import { DatosBancariosComponent } from './datos-bancarios/datos-bancarios.component';
 import { ListadoSolicitudesComponent } from './listado-solicitudes/listado-solicitudes.component';
@@ -7,7 +8,7 @@ import { SolicitudComponent } from './solicitud/solicitud.component';
 import { Solicitud2Component } from './solicitud2/solicitud2.component';
 
 const routes: Routes = [
-  {path: '', component: ListadoSolicitudesComponent},
+  {path: '', component: ListadoSolicitudesComponent, canActivate: [AuthenticationGuard]},
   {path: 'solicitud/:id', component: SolicitudComponent},
   {path: 'solicitud2', component: Solicitud2Component},
   {path: 'datos_bancarios', component: DatosBancariosComponent},
